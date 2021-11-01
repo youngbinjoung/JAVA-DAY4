e6076f97091ff9eeb945ebdedd93ed62f2561207
Youngbin Jung <71386343+youngbinjoung@users.noreply.github.com>
2021-11-01 15:47:47 +0900

    Add files via upload

---
+++ DAY 4/src/개념_배열.java
@@ -0,0 +1,8 @@
+public class °³³ä_¹è¿­ {
+	public static void main(String[] args) {
+		int[] arr= {1,2,3};
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+	}
+}
